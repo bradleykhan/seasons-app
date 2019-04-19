d61f9fdcc9633c9caff067962c1618a59e3abb22
Bradley Khan <github@bradleykhan.com>
2019-04-19 16:27:24 +0100

    Refactor app to utilise lifestyle methods and class syntatic sugar

--- src/index.js
+++ src/index.js
@@ -2,35 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    state = { lat: null, errorMessage: "" };
 
-        this.state = { lat: null, errorMessage: ''};
-
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude });
-            },
-            error => {
-                this.setState({ errorMessage: error.message });
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
         );
     }
 
     render() {
-        if(!this.state.lat && this.state.errorMessage) {
-            return <div>Error: {this.state.errorMessage}</div>
+        if (!this.state.lat && this.state.errorMessage) {
+            return <div>Error: {this.state.errorMessage}</div>;
         }
 
-        if(this.state.lat && !this.state.errorMessage) {
-            return <div>Latitude: {this.state.lat}</div>
+        if (this.state.lat && !this.state.errorMessage) {
+            return <div>Latitude: {this.state.lat}</div>;
         }
 
-        return (
-            <div>
-                Loading!
-            </div>
-        )
+        return <div>Loading!</div>;
     }
 }
 
